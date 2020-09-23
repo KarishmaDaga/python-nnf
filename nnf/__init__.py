0f73d6ed85c9ce642872d09040b32b928f5f832b
Karishma Daga <karishma.daga@queensu.ca>
2020-09-23 12:43:30 -0400

    added wip naive cnf conversion

--- nnf/__init__.py
+++ nnf/__init__.py
@@ -554,8 +554,123 @@ class NNF(metaclass=abc.ABCMeta):
 
         return neg(self)
 
-    def to_CNF(self) -> 'And[Or[Var]]':
+
+    def _to_naive_CNF(self) -> 'And[Or[Var]]':
+
+        def distribute(node):
+            
+            if isinstance(node, Var):
+                return node
+
+            elif isinstance(node, And):
+                
+                # And(Var(A), Var(B)) -> And(Var(A), Var(B))
+                if all([isinstance(child, Var) for child in node.children]):
+                    return node
+
+                else:
+                    left, right = node.children
+                    return And({distribute(left), distribute(right)})
+
+            elif isinstance(node, Or):
+
+                # Or(Var(A), Var(B)) -> Or(Var(A), Var(B))
+                if all([isinstance(child, Var) for child in node.children]):
+                    return node
+                
+                else:
+                    left, right = node.children
+
+                    # Or(And(A,B), And(C, D)) -> And(Or(A,C), Or(A,D), Or(B,C), Or(B,D))
+                    if all([isinstance(child, And) for child in node.children]):
+                        res = []
+
+                        for c in left.children:
+                            for d in right.children:
+                                res.append(Or({c, d}))
+                        return And(res)
+
+                    # Either Or(And(A,B), C) -> And(Or(A,C), Or(B,C))
+                    # or Or(C, And(A,B)) -> And(Or(C,B), Or(A,C))
+                    # where C can be Var or Or
+                    elif any([isinstance(child, And) for child in node.children]):
+
+                        if isinstance(left, And):
+                            clause1, clause2 = left.children
+                            distributed_left = Or({distribute(clause1), distribute(right)})
+                            distributed_right = Or({distribute(clause2), distribute(right)})
+
+                            return And({distribute(distributed_left), distribute(distributed_right)})
+
+                        else:
+                            clause1, clause2 = right.children
+                            distributed_left = Or({distribute(clause1), distribute(left)})
+                            distributed_right = Or({distribute(clause2), distribute(left)})
+
+                            return And({distribute(distributed_left), distribute(distributed_right)})
+
+                    # Or(Or(A, B), Or(D, C))
+                    else:
+                        left, right = node.children
+                        return Or({distribute(left), distribute(right)})
+
+        def merge_cnf(left: NNF, right: NNF) -> 'And[Or[Var]]':
+            
+            res = []
+            left_clauses = [left] if isinstance(left, Var) else [clause for clause in left.children]
+            right_clauses = [right] if isinstance(right, Var) else [clause for clause in right.children]
+
+            if len(left_clauses) > len(right_clauses):
+                for index, clause in enumerate(left_clauses):
+                    res.append((clause, right_clauses[index % len(right_clauses)]))
+            else:
+                for index, clause in enumerate(right_clauses):
+                    res.append((clause, left_clauses[index % len(left_clauses)]))
+            
+            return And(res)
+
+
+        def process_theory(node: NNF) -> 'And[Or[Var]]':
+
+            if node.is_CNF():
+                return node
+
+            res = []
+
+            if isinstance(node, Var):
+                res.append(node)
+
+            assert isinstance(node, Internal)
+
+            if len(node.children) == 1:
+                [child] = node.children
+                res.append(process_node(child))
+            
+            else:
+
+                left, right = node.children
+
+                if isinstance(node, And):
+                    left_to_cnf = distribute(left)
+                    right_to_cnf = distribute(right)
+                    return And({left_to_cnf, right_to_cnf})
+                
+                elif isinstance(node, Or):
+                    left_to_cnf = distribute(left)
+                    right_to_cnf = distribute(right)
+                    return merge_cnf(left_to_cnf, right_to_cnf)
+
+            return res
+
+        res = process_theory(self)
+        NNF._is_CNF_loose.set(res, True)
+        NNF._is_CNF_strict.set(res, True)
+        return res
+
+    def to_CNF(self, demorgan=False) -> 'And[Or[Var]]':
         """Compile theory to a semantically equivalent CNF formula."""
+        if demorgan:
+            return self._to_naive_CNF()
         return tseitin.to_CNF(self)
 
     def _cnf_satisfiable(self) -> bool:
